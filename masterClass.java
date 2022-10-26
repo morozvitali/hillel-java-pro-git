758f79d03bfbab1dc6dc2cdc72ded6d27e530507
morozvitali99 <morozvitali99@gmail.com>
2022-10-26 15:38:11 +0300

    init comit from master #2
    Added second comit from main branch #2

--- masterClass.java
+++ masterClass.java
@@ -1,5 +1,9 @@
 public class masterClass {
     public static void main(String[] args) {
-        System.out.println("comit from master");
+        System.out.println("comit from main");
+
+        System.out.println("comit from main #2");
     }
+
+
 }
