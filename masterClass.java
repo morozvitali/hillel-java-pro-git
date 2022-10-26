f90490666563600643bc7cd44d55294e107e82f1
morozvitali99 <morozvitali99@gmail.com>
2022-10-26 16:01:47 +0300

    init comit from some-feature branch #3
    Added comit from some-feature branch. Added println text to pvsm

--- masterClass.java
+++ masterClass.java
@@ -1,9 +1,7 @@
 public class masterClass {
     public static void main(String[] args) {
-        System.out.println("comit from main");
-
-        System.out.println("comit from main #2");
+        //System.out.println("comit from main");
+        //System.out.println("comit from main #2");
+        System.out.println("comit from some-feature branch #2");
     }
-
-
 }
